f705d6b9fd677ff9928e09032577f241ddcc29fb
Adam Reis <adambuczynski@gmail.com>
2016-12-06 18:09:13 +1300

    Build process (WIP)

---
+++ app/components/app/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import Home from './components/home.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Home
+  },
+}
+</script>
--- build/serve.js
+++ build/serve.js
@@ -6,7 +6,6 @@
 const path = require('path');
 const browserSync = require('browser-sync').create();
 const serveStatic = require('serve-static');
-const proxy = require('http-proxy-middleware');
 const config = require('./config');
 
 /**
@@ -14,13 +13,11 @@ const config = require('./config');
  */
 const PORT = config.PORT;
 const BASE_DIR = config.DEST_BUILD;
-const API_PROXY = config.API_PROXY;
 
 /**
  * Middleware
  */
 const staticMiddleware = serveStatic(path.resolve(BASE_DIR));
-const apiMiddleware = proxy('/api', {target: API_PROXY});
 const spaMiddleware = function(req, res, next) {
   req.url = '/index.html';
   next();
@@ -45,32 +42,7 @@ browserSync.init({
     baseDir: BASE_DIR,
     middleware: [
       staticMiddleware,
-      apiMiddleware,
       spaMiddleware,
     ],
   },
 });
-
-/**
- * Create a stream of file-change events
- */
-// Rx.Observable.create(observer => {
-//   let watcher = chokidar
-//     .watch([`${BASE_DIR}/**/*`], {ignoreInitial: true})
-//     .on('all', (event, file) => {
-//       observer.onNext({event, file});
-//     });
-//   return function() {
-//     watcher.close();
-//   };
-// })
-//   .debounce(1000)
-//   .filter(x => (x.event === 'add' || x.event === 'change'))
-//   .subscribe(x => {
-//     if (x.file.match(/\.css$/)) {
-//       browserSync.reload(x.file);
-//     }
-//     else {
-//       browserSync.reload();
-//     }
-//   });
